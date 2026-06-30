2c7af6169208720616da349ec583ac26c7cfa07e
khushi0-star <25110cn182@glbitm.ac.in>
2026-06-30 12:20:34 +0530

    Add files via upload

---
+++ day7/recursivereverse.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int reverseNumber(int n, int rev = 0)
+{
+    if (n == 0)
+        return rev;             
+
+    return reverseNumber(n / 10, rev * 10 + (n % 10)); 
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Reverse of " << n << " is " << reverseNumber(n);
+
+    return 0;
+}
